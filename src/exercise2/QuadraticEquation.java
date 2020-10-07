584cd17c5583089cbd276b915e45cad4035a7428
todortodorov0 <todor1232001@gmail.com>
2020-10-07 13:24:18 +0000

    Exercise2

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class QuadraticEquation {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.println("ax^2+bx+c = 0");
         System.out.println("a= ");
         double a= input.nextDouble();
         System.out.println("b= ");
@@ -26,8 +25,7 @@ public class QuadraticEquation {
             if(D>=0){
                 double squareRootx1=(-b+Math.sqrt(b*b-4*a*c))/2*a;
                 double squareRootx2=(-b- Math.sqrt(b * b - 4 * a * c))/2*a;
-                System.out.println("x1= "+squareRootx1);
-                System.out.println("x2= "+squareRootx2);
+                System.out.println(squareRootx1 + ", " squareRootx2);
             }else if (D<0){
                 System.out.println("Imaginary values");
             }
